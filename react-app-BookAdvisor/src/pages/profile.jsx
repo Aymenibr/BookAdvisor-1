b7bd14d92d5c616a98eb790edaca0025c1bb3c9f
Aymenibr <aynibrahim01@gmail.com>
2024-04-25 22:13:50 +0300

    done the structure

--- react-app-BookAdvisor/src/pages/profile.jsx
+++ react-app-BookAdvisor/src/pages/profile.jsx
@@ -1,10 +1,20 @@
-function profile() {
-    return (
-        <div>
-            <h1>Profile</h1> <h2>Profile22</h2>
-            <img src="my home" alt="testing" />
-            <h4>random text here</h4>
-        </div>
-    );
-}
-export default profile
+import "./profile.css";
+import book1 from "./src/imgs/Book1.jpeg";
+export const AcccountPage = ({ className, ...props }) => {
+  return (
+    <div className={"acccount-page " + className}>
+      <div className="aiman-alhaimi">AIMAN ALHAIMI </div>
+      <div className="rectangle-1"></div>
+      <img className="img-3426-1" src={book1} />
+      <img className="img-3426-2" src="img-3426-20.png" />
+      <img className="img-3432-1" src="img-3432-10.png" />
+      <img className="img-3432-2" src="img-3432-20.png" />
+      <img className="img-3429-1" src="img-3429-10.png" />
+      <img className="img-3429-2" src="img-3429-20.png" />
+      <img className="img-3427-1" src="img-3427-10.png" />
+      <img className="img-3427-2" src="img-3427-20.png" />
+      <div className="banner"></div>
+      <img className="ellipse-1" src="ellipse-10.png" />
+    </div>
+  );
+};
